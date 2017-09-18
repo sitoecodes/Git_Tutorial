ccff1745d15fed573073a50ef6c18969af75f4c8
Sitoe Thiam <sitoethiam@gmail.com>
2017-09-17 23:57:59 -0700

    Fixed code and removed extraneous file

--- impulse_response.m
+++ impulse_response.m
@@ -5,11 +5,13 @@ function [ h,t ] = impulse_response( x, y, fs )
 %           y, measured output signal
 %           fs, sampling frequency of measurements
 %   outputs:    h, calculated impulse response
+%		t, time (Sitoe Thiam, 9/17/17)
 
 [X,f] = fdomain(x,fs);
 [Y,f] = fdomain(y,fs);
 H = X./Y;   % the calculated frequency response
 [h,t] = tdomain(H,fs);
+plot(t,h);
 
 end
 
